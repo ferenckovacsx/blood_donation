57391270611462b4aa21a3b717139dafc00f5ef5
Szabó Máté <larde15@gmail.com>
2015-11-04 14:36:06 +0100

    idea has been added to the

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from donor_register import donor_register_get_user_data
 from donation_event_register import donation_event_reg
+from datetime import datetime
+from time import sleep
 
+TODAY = datetime.today()
+Switch = True
+
+while Switch:
+    print("""
+    1 = donor registration
+    2 = donor event registration
+    x = exit
+    """)
+    Answer = input("Answer: ")
+    if Answer == "1":
+        pass
+    elif Answer == "2":
+        pass
+    elif Answer == "x":
+        Switch = False
+    else:
+        print("Answer invalid.")
