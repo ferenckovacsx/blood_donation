7a9966eefd531fdbcbb79fa9f17f74d68c76a95b
BontaPeter <peter.bonta@gmail.com>
2015-11-05 11:50:36 +0100

    Donor class complete

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # blood_donation 
 
-This is a project of the Python Hussars team @ Codecool.
+This is a project of the Python Hussars team @ Codecool, written in Python.
 The objective is to organize a blood donation event. 
 
 The first part is to register a donor for the event.
@@ -12,4 +12,9 @@ The following criteria are required:
 	the donor has to be at least 18 years old
 	the donor's ID did not expire
 	the last donation date was more than 3 months ago
-	
+Data input is checked for correct format. The name must contain characters, space between the first name(s) and the
+last name(s). The weight must be an integer number. The format of birth date is like this: YYYY.MM.DD (birth date,
+last donation date, ID expiration). The ID number must be one of the following formats: 6 digits + 2 letters, or 6 letters
++ 2 digits. The blood tyoe is checked if it is in a given list. The e-mail address must contain a @ character, and has to
+end with ".hu" or ".com". The mobile number has to be in one of thee following format: +36201234567 or 06309876543-
+The first 
--- donor_class.py
+++ donor_class.py
@@ -1,1 +1,70 @@
-__author__ = 'Szabo Mate'
+from datetime import datetime
+import random
+
+MINIMUM_AGE_IN_YEAR = 18
+MINIMUM_WEIGHT_IN_KG = 50
+MIN_NUMBER_OF_DAYS_FROM_LAST_DONATION = 90
+MINIMUM_HEMOGLOBIN_LEVEL = 110
+TODAY = datetime.now()
+
+
+class Donor(object):
+    name = ""
+    weight = 0
+    gender = ""
+    birth_date = datetime(1, 1, 1)
+    last_donation = datetime(1, 1, 1)
+    was_sick_last_month = ""
+    identifier = ""
+    id_expire = datetime(1, 1, 1)
+    blodd_type = ""
+    e_mail = ""
+    mobile_number = ""
+    hemoglobin_level = 0
+    how_old = ((TODAY - birth_date).days // 365)
+
+    def check_age(self, birth_date):
+        if ((TODAY - birth_date).days // 365) > MINIMUM_AGE_IN_YEAR:
+            return True
+        else:
+            print("The minimum age is not reached.")
+            return False
+
+
+    def check_weight(self, weight):
+        if weight > MINIMUM_WEIGHT_IN_KG:
+            return True
+        else:
+            print("The minimum weight is not reached.")
+            return False
+
+
+    def check_last_donation(self, last_doantion):
+        if (TODAY - last_doantion).days > MIN_NUMBER_OF_DAYS_FROM_LAST_DONATION:
+            return True
+        else:
+            print("The minimum age is not reached.")
+            return False
+
+
+    def check_id_expiration(self, id_expire):
+        if id_expire > TODAY:
+            return True
+        else:
+            print("Your ID is expired.")
+            return False
+
+
+    def check_hemoglobin(self):
+        self.hemoglobin_level = random.randrange(80, 220)
+        if self.hemoglobin_level > 110:
+            print("Your hemoglobin level is OK.")
+            return True
+        else:
+            print("Your hemoglobin level is low.")
+
+
+    def __repr__(self):
+        how_old = ((TODAY - self.birth_date).days // 365)
+        return "%s\n%d kg\n%s - %d years old\n%s\n%s\n%s"  \
+               % (self.name, self.weight, self.birth_date.date(), how_old, self.e_mail, self.mobile_number, self.blodd_type)
