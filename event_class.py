38b372384a9734348614c01b7dd46bb906a190a1
Skoda András <skodabandi@gmail.com>
2015-11-05 14:44:27 +0100

    create def

--- event_class.py
+++ event_class.py
@@ -2,6 +2,8 @@
 from donation_event_register import donation_event_reg
 from datetime import datetime
 MIN_PERIOD = 10
+PREPARATION_TIME = 30
+DONATION_TIME = 30
 
 
 class DonationEvent:
@@ -20,10 +22,23 @@ class DonationEvent:
         self.bed_count = bed_count
         self.donor_number = donor_number
 
+
     def check_min_period(self):
-        if not datetime.today() - datetime.strptime(self.date_of_event_string, "%Y.%m.%d") >= MIN_PERIOD:
-            print("The registration should be at least 10 days before event")
+        if not datetime.strptime(self.date_of_event_string, "%Y.%m.%d") - datetime.today() >= MIN_PERIOD:
+            print("The registration should be at least 10 days before event.")
         else:
             return True
 
-    def
+    def not_weekend(self):
+        if not datetime.isoweekday(self.date_of_event_string):
+            print("Event should be on weekdays only.")
+        else:
+            return True
+
+    def donation_duration(self):
+        duration = datetime.strptime(self.donation_end_time) - datetime.strptime(self.donation_start_time)
+        return duration
+
+    def donor_number(self):
+        max_donor_number = ((self.duration - PREPARATION_TIME) / DONATION_TIME) * self.bed_count
+
